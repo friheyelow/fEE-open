fc70b5685c140648e2a7ebe531676d72e40b0b8a
NayeonJung <friheyelow@kaist.ac.kr>
2021-08-29 02:06:42 +0900

    basic form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,92 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center" justify="center" align="center">
+      <v-col cols="4">
+        <v-img
+          :src="require('../assets/kaistee.svg')"
+          class="my-3"
+          contain
+          height="100"
+        />
+      </v-col>
+      <v-col cols="4">
+        <v-img
+          :src="require('../assets/넙죽이-01.png')"
+          class="my-3"
+          contain
+          height="100"
+        />
+      </v-col>
+      <v-col cols="4">
+        <v-btn block round height="50">
+          Contact
+        </v-btn>
+      </v-col>
+
+      <v-col>
+        <h1 class="display-2 font-weight-bold mb-4">
+          전자과 과비 납부 확인
+        </h1>
+        <p class="subheading font-weight-regular">
+          과비 납부는 항시 가능하며,
+          <br>재학 기간 중 한 번만 납부하시면
+          <br>*행사 참여, 경품 당첨* 등 전자과의 혜택을 200% 누리실 수 있습니다.
+          <br>우리 1002-455-310519 이훈준
+          <br>입금자명: '이름+학번뒷5자리'  (ex. '훈준90111')
+          <br>금액: 3만원
+        </p>
+      </v-col>
+    </v-row>
+    <v-row class="text-center" >
+      <v-col>
+        <v-text-field
+          v-model="name"
+          label="이름"
+          clearable
+          outlined
+        >
+        </v-text-field>
+      </v-col>
+
+      <v-col
+
+      >
+        <v-text-field
+          v-model="st_number"
+          label="학번 (8자리)"
+          clearable
+          outlined
+        >
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <v-row class="text-center" >
+      <v-col class="mx-1">
+        <v-btn 
+          :disabled="isBtnDisabled"
+          block
+          color="primary"
+        >조회하기</v-btn>
+      </v-col>
+      
+    </v-row>
+
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    data(){
+      return {
+        name: null,
+        st_number: null,
+      }
+    },
+    computed: {
+      isBtnDisabled(){
+        return !(this.name && this.st_number && this.st_number.length==8)
+      },
+    }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
